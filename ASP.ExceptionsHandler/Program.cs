75d912bfb5ce692a10f545aa93e7663b1f1ffbc6
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-10-28 15:37:47 -0400

    refactor (Program.cs): divided Program.cs logic into StartupExtensions.cs

--- ASP.ExceptionsHandler/Program.cs
+++ ASP.ExceptionsHandler/Program.cs
@@ -1,60 +1,23 @@
-using ASP.ExceptionsHandler.Infrastructure.ExceptionHandling;
-using ASP.ExceptionsHandler.Services;
-using DotNetEnv;
+using ASP.ExceptionsHandler;
 using Serilog;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Env.Load();
-
         Log.Logger = new LoggerConfiguration()
             .WriteTo.Console()
             .CreateLogger();
         try
         {
             Log.Information("starting server.");
+
             var builder = WebApplication.CreateBuilder(args);
-            builder.Host.UseSerilog((context, loggerConfiguration) =>
-            {
-                loggerConfiguration.WriteTo.Console();
-                loggerConfiguration.ReadFrom.Configuration(context.Configuration);
-            });
 
-            // Configurate serilog from appsettings.json
-            var logPath = Environment.GetEnvironmentVariable("LOG_PATH");
-            var serverUrl = Environment.GetEnvironmentVariable("SEQ_SERVER_URL");
-
-            // Modificar la configuración de Serilog
-            builder.Configuration["Serilog:WriteTo:0:Args:path"] = logPath;
-            builder.Configuration["Serilog:WriteTo:1:Args:serverUrl"] = serverUrl;
-
-            // Add services to the container.
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            // Register services
-            builder.Services.AddScoped<ITodoService, TodoService>();
-
-            // Register exception handler
-            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-            builder.Services.AddProblemDetails();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseSerilogRequestLogging();
-            app.UseAuthorization();
-            app.UseExceptionHandler();
-            app.MapControllers();
+            //we take the configuration from StartupExtensions.cs
+            var app = builder
+                .ConfigureServices()
+                .ConfigurePipeline();
 
             app.Run();
         }
---
+++ ASP.ExceptionsHandler/StartupExtensions.cs
@@ -0,0 +1,76 @@
+﻿using ASP.ExceptionsHandler.Infrastructure.ExceptionHandling;
+using ASP.ExceptionsHandler.Services;
+using DotNetEnv;
+using Serilog;
+
+namespace ASP.ExceptionsHandler
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(
+            this WebApplicationBuilder builder)
+        {
+            Env.Load();
+
+            builder.Host.UseSerilog((context, loggerConfiguration) =>
+            {
+                loggerConfiguration.WriteTo.Console();
+                loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+            });
+
+            // Configurate serilog from appsettings.json
+            var logPath = Environment.GetEnvironmentVariable("LOG_PATH");
+            var serverUrl = Environment.GetEnvironmentVariable("SEQ_SERVER_URL");
+
+            // Modificar la configuración de Serilog
+            builder.Configuration["Serilog:WriteTo:0:Args:path"] = logPath;
+            builder.Configuration["Serilog:WriteTo:1:Args:serverUrl"] = serverUrl;
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Register services
+            builder.Services.AddScoped<ITodoService, TodoService>();
+
+            // Register exception handler
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+            builder.Services.AddProblemDetails();
+
+            //Optional: Enable CORS
+            builder.Services.AddCors(options => options.AddPolicy(
+                "open",
+                policy => policy.WithOrigins(
+                    builder.Configuration["ApiUrl"] ?? "https://localhost:7020")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .SetIsOriginAllowed(origin => true)
+            ));
+
+            // Optional: Enable Swagger for API documentation
+            builder.Services.AddSwaggerGen();
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseCors("open");
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseSerilogRequestLogging();
+            app.UseAuthorization();
+            app.UseExceptionHandler();
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
